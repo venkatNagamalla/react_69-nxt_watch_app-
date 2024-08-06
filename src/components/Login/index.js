33419c27d8263a76702a73ee43f61fb6030944ee
venkat <shivavenkat006@gmail.com>
2024-08-06 08:44:58 +0000

    final commit

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {Component} from 'react'
 import {Redirect} from 'react-router-dom'
 import Cookies from 'js-cookie'
 import {
@@ -6,148 +6,91 @@ import {
   LoginCard,
   Logo,
   FormContainer,
-  InputContainer,
-  Label,
-  Input,
-  ShowPassContainer,
-  CheckBox,
-  ShowPassText,
   LoginButton,
   ErrorMsg,
 } from './styledComponents'
 
 import Context from '../../context/Context'
 
-const Login = props => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [showPassword, setShowPassword] = useState(false)
-  const [isError, setIsError] = useState(false)
-  const [errorMsg, setErrorMsg] = useState('')
+class Login extends Component {
+  state = {errorMsg: '', isError: false}
 
-  const onLoginSuccess = jwtToken => {
+  onLoginSuccess = jwtToken => {
     Cookies.set('jwt_token', jwtToken, {expires: 30})
-    const {history} = props
+    const {history} = this.props
     history.replace('/')
   }
 
-  const onLoginFailure = error => {
-    setIsError(true)
-    setErrorMsg(error)
+  onLoginFailure = error => {
+    this.setState({errorMsg: error, isError: true})
   }
 
-  const verifyUserDetails = async userData => {
+  onSubmit = async e => {
+    e.preventDefault()
+
+    const userDetails = {username: 'rahul', password: 'rahul@2021'}
+
     const loginApi = 'https://apis.ccbp.in/login'
     const options = {
       method: 'POST',
-      body: JSON.stringify(userData),
+      body: JSON.stringify(userDetails),
     }
     const response = await fetch(loginApi, options)
     const data = await response.json()
 
     if (response.ok) {
-      onLoginSuccess(data.jwt_token)
+      this.onLoginSuccess(data.jwt_token)
     } else {
-      onLoginFailure(data.error_msg)
+      this.onLoginFailure(data.error_msg)
     }
   }
 
-  const onSubmit = e => {
-    e.preventDefault()
-    const userData = {
-      username,
-      password,
-    }
-    verifyUserDetails(userData)
+  renderFormItems = () => {
+    const {isError, errorMsg} = this.state
+    return (
+      <FormContainer onSubmit={this.onSubmit}>
+        <LoginButton type="submit">Login</LoginButton>
+        {isError && <ErrorMsg>*{errorMsg}</ErrorMsg>}
+      </FormContainer>
+    )
   }
 
-  return (
-    <Context.Consumer>
-      {value => {
-        const {theme} = value
+  render() {
+    return (
+      <Context.Consumer>
+        {value => {
+          const {theme} = value
 
-        const bgColor = theme ? '#000000' : '#ffffff'
-        const color = theme ? '#ffffff' : '#000000'
+          const bgColor = theme ? '#000000' : '#ffffff'
+          const color = theme ? '#ffffff' : '#000000'
 
-        const renderUserName = () => (
-          <InputContainer color={color}>
-            <Label htmlFor="username">USERNAME</Label>
-            <Input
-              color={color}
-              bgColor={bgColor}
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-              placeholder="Username"
-              type="text"
-              id="username"
-            />
-          </InputContainer>
-        )
+          const jwtToken = Cookies.get('jwt_token')
+          if (jwtToken !== undefined) {
+            return <Redirect to="/" />
+          }
 
-        const renderPassword = () => (
-          <InputContainer color={color}>
-            <Label htmlFor="password">PASSWORD</Label>
-            <Input
-              color={color}
-              id="password"
-              bgColor={bgColor}
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              placeholder="Password"
-              type={showPassword ? 'text' : 'password'}
-            />
-          </InputContainer>
-        )
-
-        const renderShowPassword = () => (
-          <ShowPassContainer>
-            <CheckBox
-              onChange={() => setShowPassword(prev => !prev)}
-              id="checkbox"
-              type="checkbox"
-            />
-            <ShowPassText color={color} htmlFor="checkbox">
-              Show Password
-            </ShowPassText>
-          </ShowPassContainer>
-        )
-
-        const renderFormItems = () => (
-          <FormContainer onSubmit={onSubmit}>
-            {renderUserName()}
-            {renderPassword()}
-            {renderShowPassword()}
-            <LoginButton type="submit">Login</LoginButton>
-            {isError && <ErrorMsg>*{errorMsg}</ErrorMsg>}
-          </FormContainer>
-        )
-
-        const jwtToken = Cookies.get('jwt_token')
-        if (jwtToken !== undefined) {
-          return <Redirect to="/" />
-        }
-
-        return (
-          <MainContainer bgColor={bgColor}>
-            <LoginCard bgColor={bgColor}>
-              {theme ? (
-                <Logo
-                  src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png"
-                  alt="website logo"
-                />
-              ) : (
-                <Logo
-                  src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
-                  alt="website logo"
-                />
-              )}
-              {renderFormItems()}
-            </LoginCard>
-          </MainContainer>
-        )
-      }}
-    </Context.Consumer>
-  )
+          return (
+            <MainContainer bgColor={bgColor}>
+              <LoginCard bgColor={bgColor}>
+                {theme ? (
+                  <Logo
+                    src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png"
+                    alt="website logo"
+                  />
+                ) : (
+                  <Logo
+                    src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
+                    alt="website logo"
+                  />
+                )}
+                {this.renderFormItems()}
+              </LoginCard>
+            </MainContainer>
+          )
+        }}
+      </Context.Consumer>
+    )
+  }
 }
 
 export default Login
--- src/components/Login/styledComponents.js
+++ src/components/Login/styledComponents.js
@@ -41,47 +41,7 @@ export const Logo = styled.img`
 export const FormContainer = styled.form`
   width: 100%;
 `
-export const InputContainer = styled.div`
-  padding: 5px 0px 5px 0px;
-  margin-top: 15px;
-  color: ${props => props.color};
-`
 
-export const Label = styled.label`
-  font-size: 14px;
-  color: ${props => props.color};
-  font-weight: 410;
-  letter-spacing: 0.1px;
-`
-export const Input = styled.input`
-  width: 100%;
-  margin-top: 5px;
-  height: 38px;
-  border: 1px solid #94a3b8;
-  outline: none;
-  padding: 10px;
-  color: ${props => props.color};
-  font-size: 14px;
-  border-radius: 1px;
-  background-color: ${props => props.bgColor};
-`
-
-export const ShowPassContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 12px;
-`
-export const CheckBox = styled.input`
-  height: 17px;
-  width: 17px;
-  cursor: pointer;
-`
-export const ShowPassText = styled.label`
-  color: ${props => props.color};
-  font-weight: 410;
-  margin-left: 5px;
-  cursor: pointer;
-`
 export const LoginButton = styled.button`
   background-color: #3b82f6;
   border: none;
